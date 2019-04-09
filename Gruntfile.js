d84016f6cdb9e469a3e920a1f8f4e0913a8f13e2
lixiaofan1997 <773515092@qq.com>
2019-04-09 21:20:52 +0800

    05jsnotepad 关于对话框

---
+++ Gruntfile.js
@@ -0,0 +1,87 @@
+/* global module: true */
+module.exports = function (grunt) {
+  grunt.initConfig({
+    eslint: {
+      options: {
+        configFile: '.eslintrc.json'
+      },
+      target: ['*.js']
+    },
+    csslint: {
+      options: {
+        csslintrc: '.csslintrc'
+      },
+      src: '*.css'
+    },
+    htmlhint: {
+      options: {
+        htmlhintrc: '.htmlhintrc'
+      },
+      src: '*.html'
+    },
+    mocha: {
+      test:{
+        src:['./test/index.html']      
+      },   
+      options:{
+        run:true,
+        reporter:'Spec'                        
+      }                       
+    },
+    htmlmin: {
+      options: {
+        collapseWhitespace: true,
+        preserveLineBreaks: false                        
+      },
+      files: {
+        src: './index.html',
+        dest: './dist/index.html'
+      }
+    },
+    cssmin: {
+      'dist/jsnotepad.css': 'jsnotepad.css'            
+    },
+    uglify: {
+      release:{
+        files: {
+          'dist/bundle.min.js': 'dist/bundle.js',
+                                    
+        }             
+      }
+    },
+    useminPrepare: {
+      html: 'index.html',
+      options: {
+        dest: 'dist'                  
+      }        
+    },
+    usemin: {
+      html: ['dist/index.html']           
+    },
+    concat: {
+      options: {
+        separator: ';'                  
+      },
+      js: {
+        src: ['jsnotepad.js', 'calc.js'],
+        dest: 'dist/bundle.js'                               
+      }
+          
+    },
+    clean: ['dist/bundle.js', '.tmp'],
+  });
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
+  grunt.registerTask('test', ['mocha']);
+  grunt.registerTask('release', [ 'useminPrepare', 'concat', 'uglify', 'usemin', 'cssmin', 'htmlmin', 'clean']);
+};
