f44997149dfaae76955edd20637a5a75b82287d5
lixiaofan1997 <773515092@qq.com>
2019-04-15 16:34:20 +0800

    任务7jsnotepad 查找替换对话框

---
+++ dlg-replace/dlg-replace.js
@@ -0,0 +1,36 @@
+/*exported $dlgReplace*/
+var $dlgReplace = (function(){
+var DOM=''
+    +'<div class="notepad-replace-dlg">'
+    +'<div class="dialogbox">'
+    +'<div class="titlebar">'
+    +'<p class="title">查找</p>'
+    +'<span class="btn-close">×</span>'
+    +'</div>'
+    +'<div class="content">'
+    +'<label>查找内容(N):</label>'
+    +'<input type="text" class="txt" autofocus>'
+    +'<input type="button" value="查找下一个" class="btn-next" disabled/>'
+    +'<label for="">替换为(P):</label>'
+    +'<input type="text"/ class="txt">'
+    +'<input type="button" value="替换" disabled/>'
+    +'<input type="checkbox" class="checkbox"><span>区分大小写(C)</span>'
+    +'<input type="button" value="全部替换" class="btn-rep" disabled/>'
+    +'<input type="button" value="取消" class="btn-cancel"/>'
+    +'</div>'
+    +'</div>'
+    +'</div>';
+  
+  var $dlg = $(DOM),
+      $btnClose = $dlg.find('.btn-close'),
+      $btnCancel = $dlg.find('.btn-cancel');
+  var  show=function(){
+    $('body').append($dlg);
+   
+    $dlg.find('.dialogbox').draggable({handle:$dlg.find('.titlebar')});
+    $btnClose.click(function(){$dlg.remove();});
+    $btnCancel.click(function(){$dlg.remove();});
+  };
+  return {show:show};
+
+}());  
