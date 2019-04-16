4318a1253700df4fd4b0566afa9a665bfc96402a
lixiaofan1997 <773515092@qq.com>
2019-04-16 16:47:33 +0800

    dlg-goto 修改

--- dlg-goto/dlg-goto.js
+++ dlg-goto/dlg-goto.js
@@ -9,7 +9,7 @@ var $dlgGoto = (function(){
     +'    </div>'
     +'    <div class="content">'
     +'      <label for="">行号(L):</label>'
-    +'      <input type="text" class="line-num"/>'
+    +'      <input type="text" class="line-num" autofocus/>'
     +'      <input type="button" class="btn-goto" value="转到" />'
     +'      <input type="button" class="btn-cancel" value="取消" />'
     +'    </div>'
@@ -18,15 +18,49 @@ var $dlgGoto = (function(){
   
   var $dlg = $(DOM),
       $btnCancel = $dlg.find('.btn-cancel'),
-      $btnClose = $dlg.find('.btn-close');
-
-  function show(){
-    $('body').append($dlg);
-   
-    $dlg.find('.dialogbox').draggable({handle:$dlg.find('.titlebar')});
-    $btnClose.click(function(){$dlg.remove();});
-    $btnCancel.click(function(){$dlg.remove();});
+      $btnClose = $dlg.find('.btn-close'),
+      $btnGoto = $dlg.find('.btn-goto'),
+      $txtLineNum = $dlg.find('.line-num'),
+      $titlebar = $dlg.find('.titlebar');
+  var cfg={
+    curLine:3,
+    totalLines:1,
+    gotoHandler:null
   }
-  return {show:show};
+  function destroy(){$dlg.remove();}
+
+  function validation(){
+    var num=Number($txtLineNum.val());
+    if(num === 0 || num>cfg.totalLines){
+      alert('行数超过了总行数');
+      return false;
+    }
+    return true;
+  }
+  function gotoHandler(){
+    if(!validation()) return;
+
+    cfg.gotoHandler($txtLineNum.val());
+    destroy();
+  }
+  function filterKey(e){
+    if(!/[0-9]/.test(e.key)){
+      e.preventDefault();
+      alert('不能输入除数字以外的非法字符');
+    }
+  }
+  var init=function(conf){
+    $.extend(cfg,conf);
+    $('body').append($dlg);
+    
+    $dlg.find('.dialogbox').draggable({handle:$titlebar});
+    $txtLineNum.val(cfg.curLine);
+    $btnClose.click(destroy);
+    $btnCancel.click(destroy);
+    $btnGoto.click(gotoHandler);
+    $txtLineNum.keypress(filterKey);
+    $txtLineNum.select();
+  };
+  return {init:init};
 
 }());
