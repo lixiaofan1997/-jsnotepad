21ea5d5e6c88160e336b3ae76282444e9362718d
lixiaofan1997 <773515092@qq.com>
2019-04-17 21:19:55 +0800

    对话框任务列表

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,87 +1,74 @@
 /* global module: true */
-module.exports = function (grunt) {
+module.exports = function(grunt){
   grunt.initConfig({
-    eslint: {
-      options: {
-        configFile: '.eslintrc.json'
-      },
-      target: ['*.js']
-    },
-    csslint: {
-      options: {
-        csslintrc: '.csslintrc'
-      },
-      src: '*.css'
-    },
-    htmlhint: {
-      options: {
-        htmlhintrc: '.htmlhintrc'
-      },
-      src: '*.html'
-    },
-    mocha: {
-      test:{
-        src:['./test/index.html']      
-      },   
+    eslint:{
       options:{
-        run:true,
-        reporter:'Spec'                        
-      }                       
-    },
-    htmlmin: {
-      options: {
-        collapseWhitespace: true,
-        preserveLineBreaks: false                        
+        configFile:'.eslintrc.json'
       },
-      files: {
-        src: './index.html',
-        dest: './dist/index.html'
+      target:[
+        '*.js']
+    },
+    csslint:{
+      options:{
+        csslintrc:'.csslintrc'
+      },
+      src:['*.js']
+    },
+    htmlhint:{
+      build:{
+      options:{
+        htmlhintrc:'.htmlhintrc'
+      },
+      src:['*.html']
       }
+    }  ,
+    htmlmin: {
+      dist:{
+        options: {
+          collapseWhitespace: true,
+          preserveLineBreaks: false                           
+        },
+        files: {
+          'dist/index.html':'index.html',
+          'dist/dlg-about.html':'./dlg-about/index.html',
+          'dist/dlg-goto.html':'./dlg-goto/index.html',
+          'dist/dlg-search.html':'./dlg-search/index.html',      
+          'dist/dlg-replace.html':'./dlg-replace/index.html',
+          'dist/dlg-font.html':'./dlg-font/index.html'
+         }                   
+     }  
     },
     cssmin: {
-      'dist/jsnotepad.css': 'jsnotepad.css'            
+      target:{
+      files:{ 
+          'dist/dlg-about.css':'./dlg-about/dlg-about.css',
+          'dist/dlg-goto.css':'./dlg-goto/dlg-goto.css',
+          'dist/dlg-search.css':'./dlg-search/dlg-search.css',      
+          'dist/dlg-replace.css':'./dlg-replace/dlg-replace.css',
+          'dist/dlg-font.css':'./dlg-font/dlg-font.css'
+      }
+     }       
     },
     uglify: {
       release:{
         files: {
-          'dist/bundle.min.js': 'dist/bundle.js',
-                                    
-        }             
-      }
-    },
-    useminPrepare: {
-      html: 'index.html',
-      options: {
-        dest: 'dist'                  
+          'dist/dlg-about.js':'./dlg-about/dlg-about.js',
+          'dist/dlg-goto.js':'./dlg-goto/dlg-goto.js',
+          'dist/dlg-search.js':'./dlg-search/dlg-search.js',      
+          'dist/dlg-replace.js':'./dlg-replace/dlg-replace.js',
+          'dist/dlg-font.js':'./dlg-font/dlg-font.js'
+
+        },
       }        
-    },
-    usemin: {
-      html: ['dist/index.html']           
-    },
-    concat: {
-      options: {
-        separator: ';'                  
-      },
-      js: {
-        src: ['jsnotepad.js', 'calc.js'],
-        dest: 'dist/bundle.js'                               
-      }
-          
-    },
-    clean: ['dist/bundle.js', '.tmp'],
+    }                
+  
   });
+
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-htmlhint');
   grunt.loadNpmTasks('grunt-eslint');
-  grunt.loadNpmTasks('grunt-mocha');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-usemin');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
-  grunt.registerTask('test', ['mocha']);
-  grunt.registerTask('release', [ 'useminPrepare', 'concat', 'uglify', 'usemin', 'cssmin', 'htmlmin', 'clean']);
+  grunt.registerTask('default', ['htmlhint', 'csslint', 'eslint','uglify','cssmin', 'htmlmin']);
 };
